b015b9d2218c2df835bd6bec66597a2e746e50ec
Dariusz Okonski <darek200180@gmail.com>
2019-04-04 23:06:45 +0100

    Updates

---
+++ precache-manifest.d83c09c4c0d42fe2d1860705583b20e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3598432f294fc2f91f5a",
+    "url": "/todoapp/./static/js/runtime~main.3598432f.js"
+  },
+  {
+    "revision": "3afbe8859dcca065bbd8",
+    "url": "/todoapp/./static/js/main.3afbe885.chunk.js"
+  },
+  {
+    "revision": "e65d2ef46aa8be294d01",
+    "url": "/todoapp/./static/js/2.e65d2ef4.chunk.js"
+  },
+  {
+    "revision": "3afbe8859dcca065bbd8",
+    "url": "/todoapp/./static/css/main.bbd1db13.chunk.css"
+  },
+  {
+    "revision": "0345353a72e488603f32d7fc384e7f44",
+    "url": "/todoapp/./index.html"
+  }
+];
